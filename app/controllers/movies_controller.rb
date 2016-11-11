8f9e55da41bc7b07375858a0cf0a3fc96f352e43
AlexJ <ajaksic411@gmail.com>
2016-11-11 08:17:47 +0000

    end of chapter 4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,9 @@ class MoviesController < ApplicationController
         id = params[:id] # retrieve movie ID from URI route
         @movie = Movie.find(id) # look up movie by unique ID
         # will render app/views/movies/show.html.haml by default
+    rescue
+        flash[:notice] = "Movie with a given ID could not be found."
+        redirect_to movies_path
     end
     
     def new
@@ -15,7 +18,7 @@ class MoviesController < ApplicationController
     
     def create
         params.require(:movie)
-        permitted = params[:movie].permit(:title,:rating,:release_date)
+        permitted = params[:movie].permit(:title,:rating,:release_date,:description)
         @movie = Movie.create!(permitted)
         flash[:notice] = "#{@movie.title} was successfully created."
         redirect_to movie_path(@movie)
@@ -27,7 +30,7 @@ class MoviesController < ApplicationController
     
     def update
         @movie = Movie.find params[:id]
-        permitted = params[:movie].permit(:title, :rating, :release_date)
+        permitted = params[:movie].permit(:title, :rating, :release_date,:description)
         @movie.update_attributes!(permitted)
         respond_to do |client_wants|
             client_wants.html {redirect_to movie_path(@movie)}
